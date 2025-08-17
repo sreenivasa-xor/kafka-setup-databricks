335c8806c1b77baae64636e632753fd897402870
Sreenivasa Reddy Gopireddy <53431129+sreenivasa-xor@users.noreply.github.com>
2025-08-17 12:44:49 +0530

    Update kafka_install.py

--- kafka-poc/kafka_install.py
+++ kafka-poc/kafka_install.py
@@ -4,14 +4,14 @@
 
 # COMMAND ----------
 
-# MAGIC %sh wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.12-3.1.0.tgz
+# MAGIC %sh wget https://dlcdn.apache.org/kafka/3.9.0/kafka_2.12-3.9.0.tgz
 
 # COMMAND ----------
 
-# MAGIC %sh tar -xzf kafka_2.12-3.1.0.tgz
+# MAGIC %sh tar -xzf kafka_2.12-3.9.0.tgz
 
 # COMMAND ----------
 
-# MAGIC %sh cd kafka_2.12-3.1.0/
+# MAGIC %sh cd kafka_2.12-3.9.0/
 # MAGIC ls -lrt ./
 # MAGIC bin/zookeeper-server-start.sh config/zookeeper.properties
