c4e2622b02fe60fbb1824fe898794a41599e6b55
Sreenivasa Reddy Gopireddy <53431129+sreenivasa-xor@users.noreply.github.com>
2025-08-17 12:45:04 +0530

    Update Kafka_service_start.py

--- kafka-poc/Kafka_service_start.py
+++ kafka-poc/Kafka_service_start.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %sh ls -ltr
-# MAGIC cd kafka_2.12-3.1.0
+# MAGIC cd kafka_2.12-3.9.0
 # MAGIC bin/kafka-server-start.sh config/server.properties
 
 # COMMAND ----------
