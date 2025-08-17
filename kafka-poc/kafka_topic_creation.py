1df5803f53ff73e3e1e17e44ef39a5cdcc02f335
Sreenivasa Reddy Gopireddy <53431129+sreenivasa-xor@users.noreply.github.com>
2025-08-17 12:45:35 +0530

    Update kafka_topic_creation.py

--- kafka-poc/kafka_topic_creation.py
+++ kafka-poc/kafka_topic_creation.py
@@ -1,17 +1,17 @@
 # Databricks notebook source
 # MAGIC %sh 
-# MAGIC cd kafka_2.12-3.1.0
+# MAGIC cd kafka_2.12-3.9.0
 # MAGIC bin/kafka-topics.sh --create --topic azdbadftopic --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 
 
 # COMMAND ----------
 
 # MAGIC %sh 
-# MAGIC cd kafka_2.12-3.1.0
+# MAGIC cd kafka_2.12-3.9.0
 # MAGIC bin/kafka-topics.sh --create --topic azdbadftopic1 --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 
 
 # COMMAND ----------
 
-# MAGIC %sh cd kafka_2.12-3.1.0
+# MAGIC %sh cd kafka_2.12-3.9.0
 # MAGIC bin/kafka-topics.sh --list --bootstrap-server localhost:9092
 
 # COMMAND ----------
